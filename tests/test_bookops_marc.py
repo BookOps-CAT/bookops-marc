9b2bde82cd0b843abf0e304da55d3395f2a0bcf7
Tomek <klingaroo@gmail.com>
2021-08-19 10:52:47 -0400

    top imports

--- tests/test_bookops_marc.py
+++ tests/test_bookops_marc.py
@@ -1,5 +1,21 @@
+import pytest
+
 from bookops_marc import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_Bib_top_import():
+    try:
+        from bookops_marc import Bib
+    except ImportError:
+        pytest.fail("Top level Bib import failed.")
+
+
+def test_SierraBibReader_top_import():
+    try:
+        from bookops_marc import SierraBibReader
+    except ImportError:
+        pytest.fail("Top level SierraBibReader failed.")
