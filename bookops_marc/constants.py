a33c1c9ea6284f2ecc1d6fd01e7c45dd3c69bae8
klinga <klingaroo@gmail.com>
2022-04-16 11:00:36 -0400

    adds supported thesauri & supported subject tags lists

---
+++ bookops_marc/constants.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+SUPPORTED_THESAURI = ["lcsh", "fast", "gsafd", "lcgft", "lctgm", "bookops", "homoit"]
+
+SUPPORTED_SUBJECT_TAGS = ["600", "610", "611", "630", "648", "650", "651", "655"]
