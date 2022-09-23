4269b08e56739d1836a42f215d236f15fa56625b
Tomek <klingaroo@gmail.com>
2022-09-23 11:43:03 -0400

    order init

--- bookops_marc/order.py
+++ bookops_marc/order.py
@@ -1,22 +1,86 @@
-from typing import Generator, Optional, Union
+from typing import Generator, List, Optional, Union
 
 from pymarc import Record, Field
 
-from bookops_marc.bib import Bib
-from bookops_marc.models import Order
+
+def get_branch_code(location_code: str) -> str:
+    """
+    Returns branch code from normalized location code
+    """
+    branch = location_code[:2]
+    return branch
+
+
+def get_shelf_code(location_code: str) -> Optional[str]:
+    """
+    Parses shelf code from given normalized location_code
+    """
+    try:
+        shelf = location_code[3:5].strip()
+        if shelf:
+            return shelf
+        else:
+            return None
+    except (TypeError, IndexError):
+        return None
+
+
+def normalize_location_code(code: str) -> str:
+    """
+    Removes any quantity designation from location code value
+    """
+    try:
+        s = code.index("(")
+        e = code.index(")")
+        return f"{code[:s]}{code[e + 1:]}"
+    except ValueError:
+        return code
+
+
+def normalize_order_number(order_number: str) -> int:
+    """
+    Normalizes Sierra order number
+
+    Args:
+        order_number:               Sierra full order number including a prefix and digit check
+
+    Returns:
+        int
+    """
+    return int(order_number[2:-1])
+
+
+def normalize_vendor_note(ven_note: str) -> Optional[str]:
+    pass
+
+
+def get_shelf_audience_code(location_code: str) -> Optional[str]:
+    """
+    Parses audience code from given normalized location_code
+    """
+    try:
+        audn = location_code[2].strip()
+        if audn:
+            return audn
+        else:
+            return None
+
+    except IndexError:
+        return None
 
 
 class Order:
-    def __init__(self, library: str, f960: Field, f961: Optional[Field]) -> None:
+    def __init__(self, library: str, f960: Field, f961: Optional[Field] = None) -> None:
         """
         Instates Order object
         """
 
-        self.f960 = f960
-        self.f961 = f961
+        self._960 = f960
+        self._961 = f961
         self.audn = None
 
         # fixed fields
+        self.oid = None
         self.code1 = None
         self.code2 = None
         self.code3 = None
@@ -42,8 +106,31 @@ class Order:
 
         self._parse_order_fields()
 
+    def _get_branches(self, field: Field) -> List[str]:
+        """
+        Returns isolated from location codes branches as a list
+
+        Args:
+            field:                  pymarc.Field instance
+        """
+        branches = []
+
+        for sub in field.get_subfields("t"):
+
+            # remove any qty data
+            loc_code = normalize_location_code(sub)
+
+            branch = get_branch_code(loc_code)
+            branches.append(branch)
+
+        return branches
+
     def _parse_order_fields(self):
-        pass
+        self.oid = normalize_order_number(self._960["z"])
+        try:
+            self.vendorNote = normalize_vendor_note(self._961["h"])
+        except (TypeError, KeyError):
+            pass
 
     def unique_branches(self):
         pass
--- tests/test_order.py
+++ tests/test_order.py
@@ -3,7 +3,62 @@ from contextlib import nullcontext as does_not_rise
 from pymarc import Field
 import pytest
 
-from bookops_marc.order import get_orders, Order
+from bookops_marc.order import (
+    Order,
+    get_branch_code,
+    get_shelf_audience_code,
+    get_shelf_code,
+    normalize_location_code,
+    normalize_order_number,
+)
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        ("(3)sn", "sn"),
+        ("(2)btj0f", "btj0f"),
+        ("41anf(5)", "41anf"),
+        ("41anf", "41anf"),
+        ("(3)snj0y", "snj0y"),
+    ],
+)
+def test_normalize_location_code(arg, expectation, stub_bib):
+    assert normalize_location_code(arg) == expectation
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [("41anf", "41"), ("02jje", "02"), ("snj0f", "sn"), ("fty0n", "ft")],
+)
+def test_get_branch_code(arg, expectation):
+    assert get_branch_code(arg) == expectation
+
+
+@pytest.mark.parametrize(
+    "arg,expectation", [("41", None), ("41anf", "a"), ("snj0y", "j"), ("sn   ", None)]
+)
+def test_get_shelf_audience_code(arg, expectation):
+    assert get_shelf_audience_code(arg) == expectation
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        ("41anf", "nf"),
+        ("13anb", "nb"),
+        ("snj0f", "0f"),
+        ("tb", None),
+        ("tb   ", None),
+        (None, None),
+    ],
+)
+def test_get_shelf_code(arg, expectation):
+    assert get_shelf_code(arg) == expectation
+
+
+def test_normalize_order_number():
+    assert normalize_order_number(".o28876714") == 2887671
 
 
 # def test_get_orders_invalid_library_arg(stub_bib):
