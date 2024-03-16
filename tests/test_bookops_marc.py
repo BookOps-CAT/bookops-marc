1b9357f83ea9b896a52fc0364dfbb9064aa7420d
klinga <klingaroo@gmail.com>
2024-03-16 14:57:01 -0400

    ignore mypy warnings

--- tests/test_bookops_marc.py
+++ tests/test_bookops_marc.py
@@ -4,18 +4,18 @@ from bookops_marc import __version__
 
 
 def test_version():
-    assert __version__ == "0.9.0"
+    assert __version__ == "0.10.0"
 
 
 def test_Bib_top_import():
     try:
-        from bookops_marc import Bib
+        from bookops_marc import Bib  # noqa: F401
     except ImportError:
         pytest.fail("Top level Bib import failed.")
 
 
 def test_SierraBibReader_top_import():
     try:
-        from bookops_marc import SierraBibReader
+        from bookops_marc import SierraBibReader  # noqa: F401
     except ImportError:
         pytest.fail("Top level SierraBibReader failed.")
