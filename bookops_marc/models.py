69eccfea321258add3581c0a4095393c0271a41f
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-12-10 13:56:58 -0500

    created Order class to replace Order namedtuple

--- bookops_marc/models.py
+++ bookops_marc/models.py
@@ -1,24 +1,120 @@
-# -*- coding: utf-8 -*-
-
 """
 Data models used by bookops-marc
 """
-from collections import namedtuple
+
+from datetime import date
+from typing import List, Optional
+from pymarc import Field
+from .local_values import normalize_date
 
 
-Order = namedtuple(
-    "Order",
-    [
-        "oid",
-        "audn",
-        "branches",
-        "copies",
-        "created",
-        "form",
-        "lang",
-        "shelves",
-        "status",
-        "venNotes",
-    ],
-    defaults=[None, None, None, None, None, None, None, None, None],
-)
+# class Item:
+#     """
+#     A class to represent an Item record
+#     """
+
+#     def __init__(self, field: Field) -> None:
+#         self.field = field
+#         self.call_no = None
+#         self.volume = None
+#         self.agency = None
+#         self.barcode = None
+#         self.location = None
+#         self.opac_message = None
+#         self.price = None
+#         self.type = None
+#         self.item_message = None
+#         self.vendor_code = None
+#         self.call_tag = None
+
+
+class Order:
+    """
+    A class to represent an Order record
+    """
+
+    def __init__(self, field: Field, following_field: Optional[Field]) -> None:
+        self.copies: Optional[int] = int(field.get(code="o", default=0))
+        self.created: Optional[date] = normalize_date(field.get(code="q"))
+        self.form: Optional[str] = field.get(code="g")
+        self.lang: Optional[str] = field.get(code="w")
+        self.locs: List[str] = self._get_location_codes(field)
+        self.oid: int = self._normalize_order_number(field)
+        self.status: Optional[str] = field.get(code="m")
+        self.venNotes: Optional[str] = self._get_venNotes(following_field)
+
+    def _get_location_codes(self, field: Field) -> List[str]:
+        """Returns location codes from 960$t"""
+        locs = []
+        for sub in field.get_subfields("t"):
+            try:
+                s = sub.index("(")
+                e = sub.index(")")
+                loc = f"{sub[:s]}{sub[e + 1:]}"
+            except (ValueError, TypeError, AttributeError):
+                loc = sub
+            if loc:
+                locs.append(loc)
+        return locs
+
+    def _get_venNotes(self, following_field: Optional[Field]) -> Optional[str]:
+        """
+        Returns vendor notes
+
+        Args:
+            following_field:
+                either an instance of pymarc.Field or None. This should
+                field that follows the 960 field used to instantiate an Order
+                object. If the 960 field was the last field in the record
+                then following_field is None.
+        Returns:
+            vendor note as a string or None if subfield h does not exist
+        """
+        if following_field and following_field.tag == "961":
+            venNotes: Optional[str] = following_field.get("h", None)
+            return venNotes
+        else:
+            return None
+
+    def _normalize_order_number(self, field: Field) -> int:
+        """Normalizes Sierra order number"""
+        order_num = field.get(code="z")
+        if not order_num:
+            raise ValueError("Order field must contain an order number (960$z)")
+        return int(order_num[2:-1])
+
+    @property
+    def audn(self) -> List[str]:
+        audns = []
+        for loc in self.locs:
+            if len(loc) >= 3:
+                audn = loc[2].strip()
+            else:
+                audn = None
+            if audn:
+                audns.append(audn)
+        return audns
+
+    @property
+    def branches(self) -> List[str]:
+        branches = []
+        for loc in self.locs:
+            if len(loc) >= 3:
+                branch = loc[:2]
+            else:
+                branch = None
+            if branch:
+                branches.append(branch)
+        return branches
+
+    @property
+    def shelves(self) -> List[str]:
+        shelves = []
+        for loc in self.locs:
+            if len(loc) >= 5:
+                shelf = loc[3:5].strip()
+            else:
+                shelf = None
+            if shelf:
+                shelves.append(shelf)
+        return shelves
---
+++ tests/test_models.py
@@ -0,0 +1,146 @@
+import datetime
+import pytest
+from bookops_marc.models import Order
+
+
+def test_Order(mock_960, stub_961):
+    order = Order(field=mock_960, following_field=stub_961)
+    assert order.audn == ["j", "j", "j", "j"]
+    assert order.branches == ["sn", "ag", "mu", "in"]
+    assert order.copies == 13
+    assert order.created == datetime.date(2021, 8, 2)
+    assert order.form == "b"
+    assert order.lang == "eng"
+    assert order.locs == ["snj0y", "agj0y", "muj0y", "inj0y"]
+    assert order.oid == 1000001
+    assert order.shelves == ["0y", "0y", "0y", "0y"]
+    assert order.status == "o"
+    assert order.venNotes == "foo"
+
+
+def test_Order_other_following_field(mock_960, stub_field):
+    order = Order(field=mock_960, following_field=stub_field)
+    assert stub_field.tag != "961"
+    assert order.audn == ["j", "j", "j", "j"]
+    assert order.branches == ["sn", "ag", "mu", "in"]
+    assert order.copies == 13
+    assert order.created == datetime.date(2021, 8, 2)
+    assert order.form == "b"
+    assert order.lang == "eng"
+    assert order.locs == ["snj0y", "agj0y", "muj0y", "inj0y"]
+    assert order.oid == 1000001
+    assert order.shelves == ["0y", "0y", "0y", "0y"]
+    assert order.status == "o"
+    assert order.venNotes is None
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        ("(3)sn", ["sn"]),
+        ("(2)btj0f", ["btj0f"]),
+        ("41anf(5)", ["41anf"]),
+        ("41anf", ["41anf"]),
+        ("(3)snj0y", ["snj0y"]),
+        (None, []),
+    ],
+)
+def test_Order_locs(arg, expectation, stub_960, stub_961):
+    stub_960.delete_subfield("t")
+    stub_960.add_subfield(code="t", value=arg)
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.locs == expectation
+
+
+def test_Order_locs_no_location(stub_960, stub_961):
+    stub_960.delete_subfield("t")
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.locs == []
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [("41", []), ("41anf", ["a"]), ("snj0y", ["j"]), ("sn   ", []), (None, [])],
+)
+def test_Order_audn(arg, expectation, stub_960, stub_961):
+    stub_960.delete_subfield("t")
+    stub_960.add_subfield(code="t", value=arg)
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.audn == expectation
+
+
+def test_Order_audn_no_location(stub_960, stub_961):
+    stub_960.delete_subfield("t")
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.audn == []
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        ("41anf", ["41"]),
+        ("02jje", ["02"]),
+        ("snj0f", ["sn"]),
+        ("fty0n", ["ft"]),
+        ("0n", []),
+        (None, []),
+    ],
+)
+def test_Order_branches(arg, expectation, stub_960, stub_961):
+    stub_960.delete_subfield("t")
+    stub_960.add_subfield(code="t", value=arg)
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.branches == expectation
+
+
+def test_Order_branches_no_location(stub_960, stub_961):
+    stub_960.delete_subfield("t")
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.branches == []
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        ("41anf", ["nf"]),
+        ("13anb", ["nb"]),
+        ("snj0f", ["0f"]),
+        ("tb", []),
+        ("tb   ", []),
+        (None, []),
+    ],
+)
+def test_Order_shelves(arg, expectation, stub_960, stub_961):
+    stub_960.delete_subfield("t")
+    stub_960.add_subfield(code="t", value=arg)
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.shelves == expectation
+
+
+def test_Order_shelves_no_location(stub_960, stub_961):
+    stub_960.delete_subfield("t")
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.shelves == []
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        (".o28876714", 2887671),
+        (".o12345678", 1234567),
+        (".o10000000", 1000000),
+    ],
+)
+def test_Order_oid(arg, expectation, stub_960, stub_961):
+    stub_960.delete_subfield("z")
+    stub_960.add_subfield(code="z", value=arg)
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.oid == expectation
+
+
+def test_Order_missing_order_number(stub_960, stub_961):
+    stub_960.delete_subfield("z")
+    stub_960.add_subfield(code="z", value=None)
+    with pytest.raises(ValueError) as exc:
+        Order(field=stub_960, following_field=stub_961)
+    assert str(exc.value) == "Order field must contain an order number (960$z)"
