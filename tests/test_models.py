a3c562cd344263b1dcbd166db149ec9a5a9b9bd8
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-12-12 10:03:16 -0500

    changed all Order attrs to read-only properties

--- tests/test_models.py
+++ tests/test_models.py
@@ -37,30 +37,14 @@ def test_Order_other_following_field(mock_960, stub_field):
 @pytest.mark.parametrize(
     "arg,expectation",
     [
-        ("(3)sn", ["sn"]),
-        ("(2)btj0f", ["btj0f"]),
-        ("41anf(5)", ["41anf"]),
-        ("41anf", ["41anf"]),
-        ("(3)snj0y", ["snj0y"]),
+        ("41", []),
+        ("41anf", ["a"]),
+        ("snj0y", ["j"]),
+        ("sn   ", []),
         (None, []),
+        (" ", []),
+        ("      ", []),
     ],
-)
-def test_Order_locs(arg, expectation, stub_960, stub_961):
-    stub_960.delete_subfield("t")
-    stub_960.add_subfield(code="t", value=arg)
-    order = Order(field=stub_960, following_field=stub_961)
-    assert order.locs == expectation
-
-
-def test_Order_locs_no_location(stub_960, stub_961):
-    stub_960.delete_subfield("t")
-    order = Order(field=stub_960, following_field=stub_961)
-    assert order.locs == []
-
-
-@pytest.mark.parametrize(
-    "arg,expectation",
-    [("41", []), ("41anf", ["a"]), ("snj0y", ["j"]), ("sn   ", []), (None, [])],
 )
 def test_Order_audn(arg, expectation, stub_960, stub_961):
     stub_960.delete_subfield("t")
@@ -84,6 +68,8 @@ def test_Order_audn_no_location(stub_960, stub_961):
         ("fty0n", ["ft"]),
         ("0n", []),
         (None, []),
+        (" ", []),
+        ("", []),
     ],
 )
 def test_Order_branches(arg, expectation, stub_960, stub_961):
@@ -102,12 +88,179 @@ def test_Order_branches_no_location(stub_960, stub_961):
 @pytest.mark.parametrize(
     "arg,expectation",
     [
+        ("1", 1),
+        ("02", 2),
+        (2, 2),
+        ("fty0n", None),
+        ("0n", None),
+        (None, None),
+        (" ", None),
+        ("      ", None),
+        ("three", None),
+    ],
+)
+def test_Order_copies(arg, expectation, stub_960, stub_961):
+    stub_960.delete_subfield("o")
+    stub_960.add_subfield(code="o", value=arg)
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.copies == expectation
+
+
+def test_Order_copies_no_subfield(stub_960, stub_961):
+    stub_960.delete_subfield("o")
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.copies is None
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        ("01-01-24", datetime.date(2024, 1, 1)),
+        ("02-02-02", datetime.date(2002, 2, 2)),
+        ("02-02-2022", datetime.date(2022, 2, 2)),
+        ("foo", None),
+        ("", None),
+        (None, None),
+    ],
+)
+def test_Order_created(arg, expectation, stub_960, stub_961):
+    stub_960.delete_subfield("q")
+    stub_960.add_subfield(code="q", value=arg)
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.created == expectation
+
+
+def test_Order_created_no_subfield(stub_960, stub_961):
+    stub_960.delete_subfield("q")
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.created is None
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        ("b", "b"),
+        ("a", "a"),
+        ("m", "m"),
+        ("p", "p"),
+        ("foo", None),
+        ("", None),
+        ("book", None),
+        ([], None),
+        (None, None),
+        (
+            (
+                "a",
+                "b",
+            ),
+            None,
+        ),
+    ],
+)
+def test_Order_form(arg, expectation, stub_960, stub_961):
+    stub_960.delete_subfield("g")
+    stub_960.add_subfield(code="g", value=arg)
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.form == expectation
+
+
+def test_Order_form_no_subfield(stub_960, stub_961):
+    stub_960.delete_subfield("g")
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.form is None
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        ("eng", "eng"),
+        ("pol", "pol"),
+        ("cze", "cze"),
+        ("spa", "spa"),
+        ("Spanish", None),
+        (1, None),
+        ("", None),
+        ([], None),
+        (None, None),
+        (
+            (
+                "spa",
+                "fre",
+            ),
+            None,
+        ),
+    ],
+)
+def test_Order_lang(arg, expectation, stub_960, stub_961):
+    stub_960.delete_subfield("w")
+    stub_960.add_subfield(code="w", value=arg)
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.lang == expectation
+
+
+def test_Order_lang_no_subfield(stub_960, stub_961):
+    stub_960.delete_subfield("w")
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.lang is None
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        ("(3)sn", ["sn"]),
+        ("(2)btj0f", ["btj0f"]),
+        ("41anf(5)", ["41anf"]),
+        ("41anf", ["41anf"]),
+        ("(3)snj0y", ["snj0y"]),
+        (None, []),
+    ],
+)
+def test_Order_locs(arg, expectation, stub_960, stub_961):
+    stub_960.delete_subfield("t")
+    stub_960.add_subfield(code="t", value=arg)
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.locs == expectation
+
+
+def test_Order_locs_no_location(stub_960, stub_961):
+    stub_960.delete_subfield("t")
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.locs == []
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        (".o28876714", 2887671),
+        (".o12345678", 1234567),
+        (".o10000000", 1000000),
+        (None, None),
+    ],
+)
+def test_Order_oid(arg, expectation, stub_960, stub_961):
+    stub_960.delete_subfield("z")
+    stub_960.add_subfield(code="z", value=arg)
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.oid == expectation
+
+
+def test_Order_oid_no_subfield(stub_960, stub_961):
+    stub_960.delete_subfield("z")
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.oid is None
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
         ("41anf", ["nf"]),
         ("13anb", ["nb"]),
         ("snj0f", ["0f"]),
         ("tb", []),
         ("tb   ", []),
         (None, []),
+        (" ", []),
+        ("      ", []),
     ],
 )
 def test_Order_shelves(arg, expectation, stub_960, stub_961):
@@ -126,21 +279,59 @@ def test_Order_shelves_no_location(stub_960, stub_961):
 @pytest.mark.parametrize(
     "arg,expectation",
     [
-        (".o28876714", 2887671),
-        (".o12345678", 1234567),
-        (".o10000000", 1000000),
+        ("2", "2"),
+        ("a", "a"),
+        ("o", "o"),
+        ("z", "z"),
+        ("foo", None),
+        ("", None),
+        ([], None),
+        (None, None),
+        (
+            (
+                "a",
+                "b",
+            ),
+            None,
+        ),
     ],
 )
-def test_Order_oid(arg, expectation, stub_960, stub_961):
-    stub_960.delete_subfield("z")
-    stub_960.add_subfield(code="z", value=arg)
+def test_Order_status(arg, expectation, stub_960, stub_961):
+    stub_960.delete_subfield("m")
+    stub_960.add_subfield(code="m", value=arg)
     order = Order(field=stub_960, following_field=stub_961)
-    assert order.oid == expectation
+    assert order.status == expectation
 
 
-def test_Order_missing_order_number(stub_960, stub_961):
-    stub_960.delete_subfield("z")
-    stub_960.add_subfield(code="z", value=None)
-    with pytest.raises(ValueError) as exc:
-        Order(field=stub_960, following_field=stub_961)
-    assert str(exc.value) == "Order field must contain an order number (960$z)"
+def test_Order_status_no_subfield(stub_960, stub_961):
+    stub_960.delete_subfield("m")
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.status is None
+
+
+@pytest.mark.parametrize(
+    "arg,expectation",
+    [
+        ("foo", "foo"),
+        ("bar", "bar"),
+        ("baz", "baz"),
+        ("", ""),
+        (None, None),
+    ],
+)
+def test_Order_venNotes(arg, expectation, stub_960, stub_961):
+    stub_961.delete_subfield("h")
+    stub_961.add_subfield(code="h", value=arg)
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.venNotes == expectation
+
+
+def test_Order_venNotes_no_subfield(stub_960, stub_961):
+    stub_961.delete_subfield("h")
+    order = Order(field=stub_960, following_field=stub_961)
+    assert order.venNotes is None
+
+
+def test_Order_venNotes_no_961(stub_960):
+    order = Order(field=stub_960, following_field=None)
+    assert order.venNotes is None
