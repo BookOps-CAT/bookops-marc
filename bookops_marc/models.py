4b3991b22b992f82785a39ef3d8e1943b842877d
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-12-12 10:16:28 -0500

    expanded docstring for Order class

--- bookops_marc/models.py
+++ bookops_marc/models.py
@@ -12,10 +12,56 @@ from .local_values import normalize_date
 
 class Order:
     """
-    A class to represent an Order record
+    A class to represent an `Order` record from a pair of MARC fields.
+    Attributes are computed from a MARC 960 field and the following field
+    its tag is 961.
+
+    Attributes:
+        audn:
+            a list of audience codes from the third character of the
+            location code
+        branches:
+            a list of branch codes from the first two characters of the
+            location code
+        copies:
+            an integer representing the number of copies on an order
+            from subfield 'o'.
+        created:
+            the date the order was created as a datetime.date object
+            from subfield 'q'
+        form:
+            the format of the materials being ordered from subfield 'g'.
+        lang:
+            the language of the materials being ordered from subfield 'w'.
+        locs:
+            a list of location codes from subfield 't'
+        oid:
+            the normalized order id as an integer from subfield 'z'
+        shelves:
+            a list of shelf locations from the fourth and fifth characters
+            of the location code
+        status:
+            the order's status from subfield 'm'
+        venNotes:
+            if the record's 960 field is followed by a 961 field, the vendor
+            notes as a string from subfield 'h' of the 961 field
     """
 
     def __init__(self, field: Field, following_field: Optional[Field]) -> None:
+        """
+        An `Order` object is instantiated from 1-2 `pymarc.Field` objects. These
+        fields are non-public attributes for the class and all other attributes
+        are computed from these fields.
+
+        Args:
+            field:
+                an instance of `pymarc.Field`
+            following_field:
+                either an instance of `pymarc.Field` or `None`. This is the
+                field that follows the 960 field used to instantiate an `Order`
+                object. If the 960 field was the last field in the record
+                then following_field is None.
+        """
         self._field = field
         self._following_field = following_field
 
